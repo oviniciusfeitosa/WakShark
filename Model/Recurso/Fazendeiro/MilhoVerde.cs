1f88e4b67594c759f528314a8779301b5a37ef46
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2016-09-27 11:13:46 -0300

    [add] Adição de variação de tipo de milho verde.

--- Model/Recurso/Fazendeiro/MilhoVerde.cs
+++ Model/Recurso/Fazendeiro/MilhoVerde.cs
@@ -3,6 +3,8 @@ namespace Model.Recurso.Fazendeiro
 {
     public class MilhoVerde : ARecurso, IFazendeiro
     {
-        public MilhoVerde() : base("MilhoVerde", 70, 3000, System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\milho_verde.png") {}
+        public MilhoVerde() : base("MilhoVerde", 70, 3000, System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\milho_verde.png") {
+            this.ListaImagens.Add(System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\milho_verde_tipo-2.png");
+        }
     }
 }
