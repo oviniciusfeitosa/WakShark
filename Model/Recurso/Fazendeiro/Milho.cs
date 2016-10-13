7011d22c61b1f248f1ebc9cd47a1133400a80532
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2016-10-13 01:33:28 -0300

    [ update ] redução de cooldown dos recursos

--- Model/Recurso/Fazendeiro/Milho.cs
+++ Model/Recurso/Fazendeiro/Milho.cs
@@ -3,6 +3,6 @@ namespace Model.Recurso.Fazendeiro
 {
     public class Milho : ARecurso, IFazendeiro
     {
-        public Milho() : base("Milho", 70, 1800, System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\milho.png") {}
+        public Milho() : base("Milho", 70, 1500, System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\milho.png") {}
     }
 }
