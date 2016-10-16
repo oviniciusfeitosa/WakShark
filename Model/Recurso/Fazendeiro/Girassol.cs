5af6c088e91ec28ea98dfee0c3a2fc2b7becef4d
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2016-10-16 18:20:42 -0200

    [ patch ] Correção de level necessário.

--- Model/Recurso/Fazendeiro/Girassol.cs
+++ Model/Recurso/Fazendeiro/Girassol.cs
@@ -3,6 +3,6 @@ namespace Model.Recurso.Fazendeiro
 {
     public class Girassol : ARecurso, IFazendeiro
     {
-        public Girassol() : base("Girassol", 75, 1500, System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\girassol.png") { }
+        public Girassol() : base("Girassol", 80, 1500, System.IO.Directory.GetCurrentDirectory() + @"\assets\imagem\recurso\fazendeiro\girassol.png") { }
     }
 }
